77206efe062dff95a1310b26c9b51f3cc34b88d3
Evgenii Zakirov <e.zakirov81@outlook.com>
2024-07-23 15:04:19 -0600

    fixed mongo

--- api/index.js
+++ api/index.js
@@ -17,10 +17,7 @@ const YEARLY_PRICE = 0.01
 const CHAIN_ID = process.env.CHAIN_ID
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err))
 
